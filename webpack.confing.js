3c59fd24670846b3bcf8a05cab2b5ef190659f95
PaunescuDragos-99 <dragospaunescu19@gmail.com>
2024-01-06 11:26:07 +0200

    fixed forecast display

--- webpack.confing.js
+++ webpack.confing.js
@@ -13,12 +13,8 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.svg$/,
-        use: ['@svgr/webpack', 'url-loader'], 
+        test: /\.svg/,
+        type: 'asset/inline'
       },
     ],
   },
